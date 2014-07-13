8dbc4ffdb43cf7d224baf797b2b949280660ecfd
KeMonté <kemonte.jones@gmail.com>
2014-07-13 12:42:32 -0400

    New Form View

---
+++ js/app.js
@@ -0,0 +1,69 @@
+var NotesApp = (function() {
+	var App = {
+		stores: {}
+	}
+
+	// Initialize localStorage Data Store
+	App.stores.notes = new Store('notes');
+
+	// Note Model
+	var Note = Backbone.Model.extend({
+		// Use localStorage datastore
+		localStorage: App.stores.notes,
+
+		initialize: function(){
+			if(!this.get('title')){
+				this.set({title: "Note @ " + Date() })
+			};
+
+			if(!this.get('body')){
+				this.set({body: "No Content"})
+			};
+		}
+	})
+
+	// Views
+	var NewFormView = Backbone.View.extend({
+		
+		events: {
+			"submit form": "createNote"
+		},
+
+		createNote: function(e){
+			var attrs = this.getAttributes(),
+				note = new Note();
+
+			note.set(attrs);
+			note.save();
+		},
+
+		getAttributes: function(){
+			return {
+				title: this.$('form [name=title]').val(),
+				body: this.$('form [name=body]').val()
+			}
+		}
+	});
+
+	window.Note = Note;
+
+	$(document).ready(function(){
+		App.views.new_form = new NewFormView({
+			el: $('#new')
+		});
+	})
+
+
+
+
+
+	return App;
+})();
+
+
+
+
+
+
+
+
